af15550eac7f2bb8158c10b384e16877f6894729
Vera Sorokina <vera.sorokina.2013@mail.ru>
2023-05-26 15:02:15 +0400

    Code

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrise = 1000;
+
+        int bonus = ticketPrise / 20;
+
+        System.out.println("Итоговый бонус:" + bonus);
+    }
+}
